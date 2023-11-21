a08789c7bdcc568aa3ad71dc258b2c70325e360c
AdrianPC <adrjeness@gmail.com>
2023-11-22 00:20:09 +0100

    Git fixed + added features: - It is not possible to add a flavor to a product with a specific ID when it is already in the cart

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -1,42 +1,90 @@
-"use client"
+import React, { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
-import React, {useState} from "react";
-import {CartProductType, SelectedImageType} from "@/app/product/[productId]/ProductDetails";
-import Image from "next/image";
+// Funkcja easing, która definiuje jak płynne będzie przewijanie
+const easeInOutQuad = (t, b, c, d) => {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};
 
-interface ProductImageProps {
-    cartProduct: CartProductType;
-    product: any;
-    handleFlavourSelect: (value: SelectedImageType) => void;
-}
+const ProductImage = ({ product }) => {
+    const [selectedImage, setSelectedImage] = useState(product.images[0].image);
+    const scrollContainer = useRef(null);
+    const [isAtTop, setIsAtTop] = useState(true);
+    const [isAtBottom, setIsAtBottom] = useState(false);
 
-const ProductImage: React.FC<ProductImageProps> = ({
-    cartProduct,
-    product,
-    handleColorSelect}) => {
+    // Sprawdzanie pozycji przewijania i ustawienie stanu przycisków
+    const checkScrollPosition = () => {
+        if (scrollContainer.current) {
+            setIsAtTop(scrollContainer.current.scrollTop === 0);
+            setIsAtBottom(scrollContainer.current.scrollTop + scrollContainer.current.offsetHeight >= scrollContainer.current.scrollHeight);
+        }
+    };
 
-    const [isSelect, setIsSelect] = useState(product.images[0].image);
+    useEffect(() => {
+        checkScrollPosition(); // Sprawdź pozycję początkową po montażu
+        // eslint-disable-next-line
+    }, []);
 
-    return <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
-        <div
-            className="flex flex-col items-center justify-center gap-4 cursor-pointer border h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
-            {product.images.map((image: SelectedImageType) => {
-                return (
-                    <div key={image.image} onClick={() => setIsSelect(image.image)}
-                         className={`relative w-[80%] aspect-square rounded border-teal-300 
-                         ${isSelect  === image.image ? 'border-[1.5px]' : 'border-none'}`}
-                    >
-                        <Image src={image.image} alt={image.image} fill className="object-contain"/>
+    const smoothScroll = (endPosition) => {
+        const startTime = Date.now();
+        const startPosition = scrollContainer.current.scrollTop;
+        const distance = endPosition - startPosition;
+        const duration = 500;
+
+        const scroll = () => {
+            const currentTime = Date.now() - startTime;
+            const timeFraction = currentTime / duration;
+            if (timeFraction < 1) {
+                const nextScrollPosition = easeInOutQuad(timeFraction, startPosition, distance, 1);
+                scrollContainer.current.scrollTop = nextScrollPosition;
+                requestAnimationFrame(scroll);
+            } else {
+                scrollContainer.current.scrollTop = endPosition;
+                checkScrollPosition(); // Aktualizacja stanu przycisków po zakończeniu animacji
+            }
+        };
+
+        requestAnimationFrame(scroll);
+    };
+
+    const scroll = (direction) => {
+        const container = scrollContainer.current;
+        if (container) {
+            const scrollAmount = 100; // Można dostosować do rozmiaru miniatur
+            const newScrollPosition = direction === 'up' ? container.scrollTop - scrollAmount : container.scrollTop + scrollAmount;
+            smoothScroll(newScrollPosition);
+        }
+    };
+
+    return (
+        <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+            <div className="col-span-1 flex flex-col items-center">
+                <button onClick={() => scroll('up')} disabled={isAtTop}>
+                    <FaChevronUp className={`cursor-pointer hover:text-green-500 ${isAtTop ? 'text-gray-300 hover:text-gray-300' : ''}`} />
+                </button>
+                <div className="overflow-hidden max-h-[500px]" ref={scrollContainer} onScroll={checkScrollPosition}>
+                    <div className="flex flex-col h-full">
+                        {product.images.map((image, index) => (
+                            <div key={index} onClick={() => setSelectedImage(image.image)}
+                                 className={`cursor-pointer p-1 mb-2 ${selectedImage === image.image ? 'border-[2px]' : ''}`}>
+                                <Image src={image.image} alt={`Product image ${index + 1}`} width={100} height={100} className="object-contain"/>
+                            </div>
+                        ))}
                     </div>
-                )
-            })}
+                </div>
+                <button onClick={() => scroll('down')} disabled={isAtBottom}>
+                    <FaChevronDown className={`cursor-pointer hover:text-green-500 ${isAtBottom ? 'text-gray-300 hover:text-gray-300' : ''}`} />
+                </button>
+            </div>
+            <div className="col-span-5 relative aspect-square">
+                <Image src={selectedImage} alt="Selected product image" layout="fill" className="object-contain"/>
+            </div>
         </div>
-        <div className="col-span-5 relative aspect-square">
-            <Image fill src={isSelect} alt={cartProduct.name}
-                   className="w-full h-full object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px]"
-            />
-        </div>
-    </div>
+    );
 };
 
 export default ProductImage;
