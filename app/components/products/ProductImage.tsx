41f86a2a7ac0bb1b41f052a71b38daa1d9f2b272
AdrianPC <adrjeness@gmail.com>
2023-11-23 00:22:12 +0100

    Added features: - Product image list displayed correctly for responsive view

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -62,15 +62,15 @@ const ProductImage = ({ product }) => {
 
     return (
         <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
-            <div className="col-span-1 flex flex-col items-center">
+            <div className="col-span-1 flex flex-col items-center max-sm:h-[285px] max-md:h-[330px] max-lg:h-[300px]">
                 <button onClick={() => scroll('up')} disabled={isAtTop}>
                     <FaChevronUp className={`cursor-pointer hover:text-green-500 ${isAtTop ? 'text-gray-300 hover:text-gray-300' : ''}`} />
                 </button>
-                <div className="overflow-hidden max-h-[500px]" ref={scrollContainer} onScroll={checkScrollPosition}>
+                <div className="overflow-hidden max-h-[300px]" ref={scrollContainer} onScroll={checkScrollPosition}>
                     <div className="flex flex-col h-full">
                         {product.images.map((image, index) => (
                             <div key={index} onClick={() => setSelectedImage(image.image)}
-                                 className={`cursor-pointer p-1 mb-2 ${selectedImage === image.image ? 'border-[2px]' : ''}`}>
+                                 className={`cursor-pointer p-1 mb-2 ${selectedImage === image.image ? 'border-[2px] box-sizing' : ''}`}>
                                 <Image src={image.image} alt={`Product image ${index + 1}`} width={100} height={100} className="object-contain"/>
                             </div>
                         ))}
@@ -81,7 +81,7 @@ const ProductImage = ({ product }) => {
                 </button>
             </div>
             <div className="col-span-5 relative aspect-square">
-                <Image src={selectedImage} alt="Selected product image" layout="fill" className="object-contain"/>
+                <Image src={selectedImage} alt="Selected product image" layout="fill" className="object-contain object-top"/>
             </div>
         </div>
     );
